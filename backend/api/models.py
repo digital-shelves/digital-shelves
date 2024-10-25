01f65ac7a8d5a35b075c1f3c28eb3008fc74ce4a
dalvarez52 <alvarez.d@ufl.edu>
2024-10-25 13:13:47 -0700

    post and get

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=100)
     checkout_date = models.DateField(auto_now_add=True)
+    description = models.CharField(max_length=500, default='')
+    amount_available = models.IntegerField(default=0)
+    img_url = models.URLField(default='127.0.0.1:8080/static/img')
